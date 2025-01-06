ec46e1124ddb50f5378e770b76429956d6497f9c
Darya Melgunova <darya.melgunova@gmail.com>
2025-01-06 14:35:35 +0300

    added screenshort attach

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,5 +1,3 @@
-package tests;
-
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
@@ -12,21 +10,18 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import java.util.Map;
 
 public class TestBase {
-
     @BeforeAll
     static void beforeAll() {
-        Configuration.browserSize = "920x920";
-        Configuration.pageLoadStrategy = "eager";
         Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1980x1080";
+        Configuration.pageLoadStrategy = "eager";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
-                "enableVNC", true,
-                "enableVideo", true
-        ));
-        Configuration.browserCapabilities = capabilities;
+
+
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
     }
+
     @AfterEach
     void afterEach() {
         Attach.screenshotAs("Last screenshot");
@@ -36,5 +31,4 @@ public class TestBase {
 
         Selenide.closeWebDriver();
     }
-
 }
