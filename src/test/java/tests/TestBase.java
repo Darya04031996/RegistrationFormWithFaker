275762424a9c131b54ed54179b16081234a7792b
Darya Melgunova <darya.melgunova@gmail.com>
2025-01-06 14:54:19 +0300

    added screenshort attach

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,6 +1,5 @@
 package tests;
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
@@ -8,14 +7,14 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 
 
+
 public class TestBase {
     @BeforeAll
     static void beforeAll() {
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "1980x1080";
+        Configuration.browserSize = "920x920";
         Configuration.pageLoadStrategy = "eager";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
-
 
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
 
@@ -28,6 +27,5 @@ public class TestBase {
         Attach.browserConsoleLogs();
         Attach.addVideo();
 
-        Selenide.closeWebDriver();
     }
 }
