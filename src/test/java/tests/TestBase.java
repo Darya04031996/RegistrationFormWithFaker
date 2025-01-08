e566c16e5a5e2aeda60082001d111cf440c038c4
Darya Melgunova <darya.melgunova@gmail.com>
2025-01-08 19:14:51 +0300

    added screenshort attach

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -6,20 +6,22 @@ import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.util.Map;
 
     public class TestBase {
-        @BeforeAll
-        static void beforeAll() {
-            // System.out.println(System.getProperties());
 
+        @BeforeAll
+        static void setupConfig() {
             Configuration.baseUrl = "https://demoqa.com";
-            Configuration.browserSize = System.getProperty("browserSize");
+            Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
             Configuration.pageLoadStrategy = "eager";
-            Configuration.remote = "https://user1:1234@" + System.getProperty("remoteHost") + "/wd/hub";
-            Configuration.browser = System.getProperty("browser");
+
+            String remoteHost = System.getProperty("remoteHost", "localhost");
+            Configuration.remote = "https://user1:1234@" + remoteHost + "/wd/hub";
+            Configuration.browser = System.getProperty("browser", "chrome");
             Configuration.browserVersion = System.getProperty("browserVersion", "120");
 
             DesiredCapabilities capabilities = new DesiredCapabilities();
@@ -28,13 +30,15 @@ import java.util.Map;
                     "enableVideo", true
             ));
             Configuration.browserCapabilities = capabilities;
+        }
 
+        @BeforeEach
+        void setupTest() {
             SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-
         }
 
         @AfterEach
-        void afterEach() {
+        void teardown() {
             Attach.screenshotAs("Last screenshot");
             Attach.pageSource();
             Attach.browserConsoleLogs();
