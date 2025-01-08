2dcca7813dcd61267c6f9617da06607811ae119d
Darya Melgunova <darya.melgunova@gmail.com>
2025-01-08 17:04:58 +0300

    added screenshort attach

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,5 +1,6 @@
 package tests;
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
@@ -9,33 +10,36 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.util.Map;
 
+    public class TestBase {
+        @BeforeAll
+        static void beforeAll() {
+            // System.out.println(System.getProperties());
 
-public class TestBase {
-    @BeforeAll
-    static void beforeAll() {
-        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "1980x1080";
-        Configuration.pageLoadStrategy = "eager";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+            Configuration.baseUrl = "https://demoqa.com";
+            Configuration.browserSize = System.getProperty("browserSize");
+            Configuration.pageLoadStrategy = "eager";
+            Configuration.remote = "https://user1:1234@" + System.getProperty("remoteHost") + "/wd/hub";
+            Configuration.browser = System.getProperty("browser");
+            Configuration.browserVersion = System.getProperty("browserVersion", "124");
 
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
-                "enableVNC", true,
-                "enableVideo", true
-        ));
-        Configuration.browserCapabilities = capabilities;
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                    "enableVNC", true,
+                    "enableVideo", true
+            ));
+            Configuration.browserCapabilities = capabilities;
 
-        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+            SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
 
+        }
+
+        @AfterEach
+        void afterEach() {
+            Attach.screenshotAs("Last screenshot");
+            Attach.pageSource();
+            Attach.browserConsoleLogs();
+            Attach.addVideo();
+
+            Selenide.closeWebDriver();
+        }
     }
-
-    @AfterEach
-    void afterEach() {
-        Attach.screenshotAs("Last screenshot");
-        Attach.pageSource();
-        Attach.browserConsoleLogs();
-        Attach.addVideo();
-
-
-    }
-}
